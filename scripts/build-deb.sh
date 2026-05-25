6efd45cda0b7ee84f6ef295637be0b9f242ba501
Minh Vu <vuhoangminh97@gmail.com>
2026-05-25 14:09:48 +0200

    Fix Debian package metadata and docs

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -8,6 +8,8 @@ DIST_DIR="${REPO_DIR}/dist"
 PKGROOT="${DIST_DIR}/pkgroot"
 DEBIAN_DIR="${PKGROOT}/DEBIAN"
 PACKAGE_PATH="${DIST_DIR}/codex-ubuntu_${VERSION}_all.deb"
+DOC_DIR="${PKGROOT}/usr/share/doc/codex-ubuntu"
+MAN_DIR="${PKGROOT}/usr/share/man/man1"
 
 command -v dpkg-deb >/dev/null 2>&1 || {
   printf 'dpkg-deb is required to build the Debian package.\n' >&2
@@ -19,11 +21,16 @@ mkdir -p \
   "$DEBIAN_DIR" \
   "${PKGROOT}/usr/bin" \
   "${PKGROOT}/usr/share/applications" \
-  "${PKGROOT}/usr/share/icons/hicolor/scalable/apps"
+  "${PKGROOT}/usr/share/icons/hicolor/scalable/apps" \
+  "$DOC_DIR" \
+  "$MAN_DIR"
 
 install -m 755 "${REPO_DIR}/launcher/codex-ubuntu" "${PKGROOT}/usr/bin/codex-ubuntu"
 install -m 644 "${REPO_DIR}/desktop/codex-ubuntu.svg" "${PKGROOT}/usr/share/icons/hicolor/scalable/apps/codex-ubuntu.svg"
 "${REPO_DIR}/scripts/render-desktop-file.sh" "/usr/bin/codex-ubuntu" "codex-ubuntu" "${PKGROOT}/usr/share/applications/codex-ubuntu.desktop"
+install -m 644 "${REPO_DIR}/packaging/deb/copyright" "${DOC_DIR}/copyright"
+gzip -n -9 -c "${REPO_DIR}/packaging/deb/changelog" >"${DOC_DIR}/changelog.gz"
+gzip -n -9 -c "${REPO_DIR}/packaging/deb/codex-ubuntu.1" >"${MAN_DIR}/codex-ubuntu.1.gz"
 
 sed "s|__VERSION__|${VERSION}|g" "${REPO_DIR}/packaging/deb/control.in" >"${DEBIAN_DIR}/control"
 install -m 755 "${REPO_DIR}/packaging/deb/postinst" "${DEBIAN_DIR}/postinst"
